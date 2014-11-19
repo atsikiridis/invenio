aadd7f14a5cc9843f86d42e566f48afa6318b83c
Artem Tsikiridis <artem.tsikiridis@cern.ch>
2014-11-19 14:33:03 +0100

    authors: initial release
    * Initial module structure.
    * Introduces SQLAlchemy models for Publication, Author and Signature.
    * Introduces claiming operations on signatures.
    * Adds citation association table.
    * Tests for models.
    
    Signed-off-by: Artem Tsikiridis <artem.tsikiridis@cern.ch>
    Co-authored-by: Gilles Louppe <gilles.louppe@cern.ch>
    Co-authored-by: Mateusz Susik <mateusz.susik@cern.ch>
    Co-authored-by: Evangelos Tzemis <evangelos.tzemis@cern.ch>

---
+++ invenio/modules/authors/models.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+#
+## This file is part of Invenio.
+## Copyright (C) 2011, 2012, 2014 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+Authors database models.
+"""
+
+from invenio.ext.sqlalchemy import db
+from invenio.modules.accounts.models import User
+from invenio.modules.records.models import Record as RecordSQL
+from invenio.modules.records.api import get_record
+from invenio.modules.authors.errors import SignatureExistsError
+
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+
+
+Citation = db.Table('authors_citation', db.metadata,
+                    db.Column('citer', db.Integer(15, unsigned=True),
+                              db.ForeignKey('authors_publication.id')),
+                    db.Column('cited', db.Integer(15, unsigned=True),
+                              db.ForeignKey('authors_publication.id')))
+
+
+class Publication(db.Model):
+
+    """Represents a publication entity."""
+
+    __tablename__ = 'authors_publication'
+
+    __mapper_args__ = {'confirm_deleted_rows': False}
+
+    id = db.Column(db.Integer(15, unsigned=True), primary_key=True,
+                   autoincrement=True, nullable=False)
+    record_sql = db.Column(db.MediumInteger(8, unsigned=True),
+                           db.ForeignKey(RecordSQL.id))
+    __secondaryjoin_q = """and_(Signature._author_id == Author.id,
+                               Signature.attribution.in_(('unknown',
+                                                          'verified')))"""
+    authors = db.relationship('Author', secondary='authors_signature',
+                              secondaryjoin=__secondaryjoin_q)
+    references = db.relationship('Publication', secondary=Citation,
+                                 primaryjoin=Citation.c.cited == id,
+                                 secondaryjoin=Citation.c.citer == id,
+                                 backref='citations')
+
+    def __repr__(self):
+        """TODO DOCSTRING."""
+        return 'Publication(id=%d)' % self.id
+
+    def delete(self):
+        """TODO."""
+        sigs = Signature.query.filter(Signature.publication == self).all()
+        for s in sigs:
+            db.session.delete(s)
+
+        db.session.delete(self)
+        db.session.commit()
+
+    @property
+    def record(self):
+        """TODO."""
+        return get_record(self.record_sql.id)
+
+
+class Author(db.Model):
+
+    """Represents an author entity."""
+
+    __tablename__ = 'authors_author'
+
+    __mapper_args__ = {'confirm_deleted_rows': False}
+
+    id = db.Column(db.Integer(15, unsigned=True), primary_key=True,
+                   autoincrement=True, nullable=False)
+    _user_id = db.Column(db.Integer(15, unsigned=True), db.ForeignKey(User.id))
+    record_sql = db.Column(db.MediumInteger(8, unsigned=True),
+                           db.ForeignKey(RecordSQL.id))
+    user = db.relationship('User')
+    __secondaryjoin_q = """and_(Signature._publication_id == Publication.id,
+                                Signature.attribution.in_(('unknown',
+                                                           'verified')))"""
+    publications = db.relationship('Publication',
+                                   secondary='authors_signature',
+                                   secondaryjoin=__secondaryjoin_q)
+
+    @property
+    def record(self):
+        """TODO."""
+        return get_record(self.record_sql.id)
+
+    @property
+    def coauthors(self):
+        """TODO."""
+        coauthors = set()
+        for publication in self.publications:
+            for author in publication.authors:
+                if author != self:
+                    coauthors.add(author)
+
+        return tuple(coauthors)
+
+    @property
+    def citations(self):
+        """TODO."""
+        citations = set()
+        map(citations.update, [pub.citations for pub in self.publications])
+        return tuple(citations)
+
+    def __repr__(self):
+        """TODO."""
+        return 'Author(id=%d)' % self.id
+
+    def delete(self):
+        """TODO."""
+        for s in Signature.query.filter(Signature.author == self).all():
+            s.author = None
+            db.session.add(s)
+
+        db.session.commit()
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Signature(db.Model):
+
+    """Represents a signature on a publication."""
+
+    __mapper_args__ = {'confirm_deleted_rows': False}
+
+    __tablename__ = 'authors_signature'
+
+    id = db.Column(db.Integer(15, unsigned=True), primary_key=True,
+                   autoincrement=True, nullable=False)
+    json = db.Column(db.JSON, default=None)
+    _publication_id = db.Column(db.Integer(15, unsigned=True),
+                                db.ForeignKey(Publication.id), nullable=False)
+    _author_id = db.Column(db.Integer(15, unsigned=True),
+                           db.ForeignKey(Author.id, ondelete="SET NULL"))
+    _curator_id = db.Column(db.Integer(15, unsigned=True),
+                            db.ForeignKey(User.id))
+    attribution = db.Column(db.Enum('unknown', 'rejected', 'verified'),
+                            default='unknown')
+
+    publication = db.relationship('Publication', backref='signatures')
+    author = db.relationship('Author', backref='signatures')
+    curator = db.relationship('User')
+
+    def __repr__(self):
+        """TODO."""
+        return 'Signature(id=%d)' % self.id
+
+    def claim(self, user):
+        """TODO."""
+        self.curator = user
+        self.attribution = 'verified'
+        db.session.add(self)
+        db.session.commit()
+
+    def disclaim(self, user):
+        """TODO."""
+        self.curator = user
+        self.attribution = 'rejected'
+        db.session.add(self)
+        db.session.commit()
+
+    def move(self, author, user=None):
+        """TODO."""
+        self.curator = user
+        self.author = author
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def reassign(author, user, signature):
+        """TODO."""
+        try:
+            Signature.query.filter_by(publication=signature.publication,
+                                      author=author).one()
+            raise SignatureExistsError("""Cannot reassign signature %s to
+            author %s for publication %s. The author already has a signature
+            for this publication.""" % (signature,
+                                        author,
+                                        signature.publication))
+        except NoResultFound:
+            signature.disclaim(user)
+            new_signature = Signature(publication=signature.publication,
+                                      author=author)
+            db.session.add(signature)
+            db.session.add(new_signature)
+            db.session.commit()
+        except MultipleResultsFound, e:
+            # This should not happen.
+            raise e
+
+
+__all__ = ['Publication', 'Author', 'Signature', 'Citation']
---
+++ invenio/modules/authors/testsuite/test_authors_models.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*-
+#
+## This file is part of Invenio.
+## Copyright (C) 2011, 2012, 2014 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+Tests for Authors Models
+"""
+from invenio.testsuite import make_test_suite, run_test_suite, InvenioTestCase
+from invenio.ext.sqlalchemy import db
+from invenio.modules.authors.models import Author, Publication, Signature
+from invenio.modules.accounts.models import User
+from invenio.modules.authors.errors import SignatureExistsError
+
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+
+
+class ConfiguredModelsSetup(object):
+
+    @classmethod
+    def prepare(cls):
+        cls._publications = [Publication() for _ in range(0, 3)]
+        cls._authors = [Author() for _ in range(0, 3)]
+
+        map(cls._publications[0].authors.append, cls._authors[0:2])
+        map(cls._publications[1].authors.append, cls._authors[1:3])
+        cls._publications[2].authors.append(cls._authors[0])
+        cls._publications[0].references.append(cls._publications[1])
+
+        map(db.session.add, cls._authors)
+        map(db.session.add, cls._publications)
+        db.session.commit()
+
+    @classmethod
+    def clean(cls):
+        for author in cls._authors:
+            author.delete()
+
+        for publication in cls._publications:
+            publication.delete()
+
+
+class TestAuthorModels(InvenioTestCase, ConfiguredModelsSetup):
+    """Base class for author model tests"""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.prepare()
+
+    def test_publication_has_author(self):
+        """Publications should have authors assigned automatically."""
+        pub_authors = self._publications[0].authors
+
+        self.assertEqual(set(pub_authors), {self._authors[0],
+                                            self._authors[1]})
+
+    def test_author_has_publication(self):
+        """Authors should have publications assigned automatically."""
+        author_pubs = self._authors[0].publications
+
+        self.assertEqual(set(author_pubs),
+                         {self._publications[0], self._publications[2]})
+
+        author_pubs = self._authors[1].publications
+
+        self.assertEqual(set(author_pubs),
+                         {self._publications[0], self._publications[1]})
+
+    def test_references_and_citations(self):
+
+        self.assertEqual(self._publications[0].references,
+                         [self._publications[1]])
+
+        self.assertEqual(self._publications[1].citations,
+                         [self._publications[0]])
+
+    def test_coauthors(self):
+        self.assertEqual(self._authors[0].coauthors,
+                         (self._authors[1],))
+
+    def test_author_citations(self):
+        self.assertEqual(self._authors[2].citations,
+                         (self._publications[0],))
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.clean()
+
+
+class TestAuthorsModelsCleanup(InvenioTestCase):
+
+    def setUp(self):
+        self._publications = [Publication() for _ in range(0, 3)]
+        self._author = Author()
+
+        map(self._author.publications.append, self._publications)
+
+        db.session.add(self._author)
+        map(db.session.add, self._publications)
+        db.session.commit()
+
+    def test_author_deletion(self):
+        """When an author is deleted, the respective signatures should
+           have NULL as author. The publications should be there"""
+        sigs = Signature.query.filter(
+            Signature.author == self._author).all()
+        self._author.delete()
+
+        self.assertEqual([None] * 3, [sig.author for sig in sigs])
+
+    def test_publication_deletion(self):
+        """When a publication is deleted the respective signatures should be
+           deleted"""
+        self._publications[0].delete()
+        deleted_pub = self._publications[0]
+
+        self.assertFalse(Signature.query.filter(
+            Signature.publication == deleted_pub).all())
+
+    def test_signature_deletion(self):
+        pub1 = self._publications[0]
+        sign1 = Signature.query.filter(Signature.publication == pub1).all()[0]
+        db.session.delete(sign1)
+        db.session.commit()
+
+        author = self._author
+        publication = self._publications[0]
+
+        self.assertTrue(author)
+        self.assertTrue(publication)
+        self.assertEqual(set(author.publications),
+                         set(self._publications[1:]))
+
+    def tearDown(self):
+        try:
+            self._author.delete()
+        except AttributeError:
+            pass
+        for publication in self._publications:
+            try:
+                publication.delete()
+            except AttributeError:
+                pass
+
+
+class TestAuthorsModelsClaims(InvenioTestCase, ConfiguredModelsSetup):
+
+    def setUp(self):
+        self.prepare()
+        pub_2 = self._publications[1]
+        self._user = User(password='test')
+        self._authors[0].publications.append(pub_2)
+        db.session.add(self._authors[0])
+        db.session.add(self._user)
+        db.session.commit()
+
+    def _get_signature(self, author, publication):
+        sig_query = Signature.query.filter(
+            db.and_(Signature.author == author,
+                    Signature.publication == publication))
+        try:
+            sig = sig_query.one()
+        except NoResultFound:
+            self.fail('No signature found for author: %s and publication %s' %
+                      (author, publication))
+        except MultipleResultsFound:
+            self.fail("""Multiple signatures found for author: %s and
+            publication %s. In the context of the test this is wrong.""" %
+                      (author, publication))
+        return sig
+
+    def test_signature_claim(self):
+        sig = self._get_signature(self._authors[0],
+                                  self._publications[1])
+        sig_id = sig.id
+        sig.claim(self._user)
+        self.assertEqual(sig.author, Signature.query.get(sig_id).author)
+        self.assertEqual(self._user,
+                         Signature.query.get(sig_id).curator)
+        self.assertEqual(Signature.query.get(sig_id).attribution,
+                         'verified')
+
+    def test_signature_disclaim(self):
+        sig = self._get_signature(self._authors[0],
+                                  self._publications[1])
+        sig_id = sig.id
+        pubs_before = len(sig.author.publications)
+        sig.disclaim(self._user)
+        self.assertEqual(pubs_before, len(sig.author.publications) + 1)
+        self.assertEqual(sig.author, Signature.query.get(sig_id).author)
+        self.assertEqual(self._user,
+                         Signature.query.get(sig_id).curator)
+        self.assertEqual(Signature.query.get(sig_id).attribution,
+                         'rejected')
+        self.assertFalse(self._publications[1] 
+                         in self._authors[0].publications)
+        self.assertFalse(self._authors[0] in self._publications[1].authors)
+
+    def test_signature_move(self):
+        sig = self._get_signature(self._authors[0],
+                                  self._publications[1])
+        sig_len_before = db.session.query(Signature).count()
+        sig.move(self._authors[1])
+        sig_len_after = db.session.query(Signature).count()
+
+        self.assertFalse(self._publications[1]
+                         in self._authors[0].publications)
+
+        self.assertTrue(self._publications[1]
+                        in self._authors[1].publications)
+            
+        self.assertEqual(sig_len_before, sig_len_after)
+
+    def test_signature_reassignment(self):
+        sig = self._get_signature(self._authors[0], self._publications[2])
+        sig_len_before = db.session.query(Signature).count()
+        Signature.reassign(self._authors[2], self._user, sig)
+        sig_len_after = db.session.query(Signature).count()
+        self.assertEqual(sig_len_before + 1, sig_len_after)
+        self.assertEqual(sig.attribution, 'rejected')
+        one_sig = Signature.query.filter_by(publication=self._publications[2],
+                                            author=self._authors[2]).one()
+        self.assertTrue(one_sig)
+
+    def test_signature_reassignment_already_assigned(self):
+        sig = self._get_signature(self._authors[0], self._publications[1])
+        try:
+            with self.assertRaises(SignatureExistsError):
+                Signature.reassign(self._authors[0], self._user, sig)
+        except AssertionError:
+            self.fail("""SignatureExistsError was not raised, 
+                when trying to associate a second signature to 
+                the same pair of Author and Publications""")                  
+
+    def tearDown(self):
+        db.session.delete(self._user)
+        db.session.commit()
+        self.clean()
+
+TEST_SUITE = make_test_suite(TestAuthorModels,)
+
+if __name__ == "__main__":
+    run_test_suite(TEST_SUITE)
+
