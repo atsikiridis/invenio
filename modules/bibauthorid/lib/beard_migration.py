d71148a8a8bccd6f26b242694696204fde61413d
Artem Tsikiridis <artem.tsikiridis@cern.ch>
2014-11-26 16:43:58 +0100

    scipified :)

--- modules/bibauthorid/lib/beard_migration.py
+++ modules/bibauthorid/lib/beard_migration.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from scipy.sparse import lil_matrix
+from scipy.sparse import lil_matrix, csr_matrix
 from itertools import groupby
 from invenio.dbquery import run_sql
 
@@ -30,40 +30,50 @@ from invenio.dbquery import run_sql
 
 
 def extract_data_from_signatures(signatures):
-    data = dict()
+    data = list()
     for sig in signatures:
-        data['name'] = sig[-2]
-        # affiliation
-        # field number
-        data['bibrec'] = sig[3]
+        data.append({'name': sig[-2],
+                     'bibrec': sig[3],
+                     'affiliation': None,
+                     'field_number': None})
 
 
 
-def authors_affinity_matrix(signatures):
-    affinity = lil_matrix((len(signatures), len(signatures)), dtype=np.int8)
+def signature_similarity(all_signatures):
+    n_signatures = len(all_signatures)
+    similarity = lil_matrix((n_signatures, n_signatures), dtype=np.int8)
     counter = 0
-    for _, sigs in groupby(signatures, lambda x: x[0]):
-        sigs = list(sigs)
-        for i, s1 in enumerate(sigs):
-            for j, s2 in enumerate(sigs[i+1:]):
-                if s1[-1] == 2 and s2[-1] == 2:
-                    affinity[counter+i, counter+j] = 1
-                    affinity[counter+j, counter+i] = 1
-                elif (s1[-1] == 2 and s2[-1] == -2) or\
-                        (s1[-1] == -2 and s2[-1] == 2):
-                    affinity[counter+i, counter+j] = -1
-                    affinity[counter+j, counter+i] = -1
 
-        for d in range(counter, counter + len(sigs)):
-            affinity[d, d] = 1
-        counter += len(sigs)
-    return affinity.tocsr()
+    for n, (personid, signatures) in enumerate(groupby(all_signatures, lambda x: x[0])):
+        if n % 1000 == 0: print n
 
-signatures = set(run_sql("""select personid, bibref_table, bibref_value,
-                            bibrec, name, flag from aidPERSONIDPAPERS  """))
+        signatures = list(signatures)
+        vector = np.zeros((len(signatures, )), dtype=np.complex)
+
+        for i, s in enumerate(signatures):
+            if s[-1] == 2:
+                vector[i] = 1
+            elif s[-1] == -2:
+                vector[i] = 1j
+
+        vector = csr_matrix(vector)
+
+        block = vector.T * vector
+        block[block == -1.] = 0
+        block[block == 1j] = -1
+        block = block.astype(np.int8)
+
+        similarity[counter:counter + len(signatures), counter:counter + len(signatures)] = block
+        counter += len(signatures)
+
+    similarity.setdiag(1)
+
+    return similarity
+
+signatures = run_sql("""select personid, flag from aidPERSONIDPAPERS """)
 no_of_sigs = len(signatures)
 signatures = sorted(signatures)
-matrix = authors_affinity_matrix(signatures)
+matrix = signature_similarity(signatures)
 
 print matrix.shape
 print matrix.nnz
